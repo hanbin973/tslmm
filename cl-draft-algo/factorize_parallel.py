3f2635e49a64bde2a3f00ca430d055b92579dade
Nate Pope <nspope@utexas.edu>
2024-04-30 21:06:00 -0700

    Streamline block storage

--- cl-draft-algo/factorize_parallel.py
+++ cl-draft-algo/factorize_parallel.py
@@ -2,6 +2,7 @@ import numba
 import numpy as np
 import scipy.sparse as sparse
 import scipy.io as io
+import time
 
 # --- lib
 
@@ -29,32 +30,37 @@ def forward_solve(Lp, Li, Lx, y):
             x[j] -= Lx[p] * x[Li[p]]
         x[j] /= Lx[Lp[j]]
 
-@numba.njit("Tuple((f8[:, :], f8[:]))(i4, i4, i4[:], i4[:], f8[:], i4[:], i4[:])")
-def _factorize_worker(block_start, block_size, Lp, Li, Lx, Zp, Zi):
+@numba.njit("void(i4, i4, i4[:], i4[:], f8[:], i4[:], i4[:], f8[:,:], f8[:])")
+def _factorize_worker(block_start, block_size, Lp, Li, Lx, Zp, Zi, P, D):
     """
     `L` is n-by-n sparse lower-triangular Cholesky factor in CSC format.
     `Z` is n-by-m sparse design matrix in CSC format.
-    Returns m-by-m dense matrix `Z (L L')^{-1} Z'`.
+
+    Diagonalize a block of `Z (LL')^{-1} Z'`.
+
+    Fills P and D so that, `P[block_start:block_start+block_size]` and
+    `D[block_start:block_start+block_size]` are the eigenvectors and
+    eigenvalues.
     """
+    n, s = block_start, block_size
     x = np.zeros(Lp.size - 1)
-    output = np.empty((block_size, block_size))
+    output = np.empty((s, s))
     ii = 0
-    for i in range(block_start, block_start + block_size):
+    for i in range(n, n + s):
         il, iu = Zp[i:i+2]
         x[Zi[il:iu]] = 1.0
         back_solve(Lp, Li, Lx, x)
         forward_solve(Lp, Li, Lx, x)
         jj = 0
-        for j in range(block_start, block_start + block_size):
+        for j in range(n, n + s):
             jl, ju = Zp[j:j+2]
             output[ii, jj] = np.sum(x[Zi[jl:ju]])
             jj += 1
         x[:] = 0.0
         ii += 1
-    D, P = np.linalg.eig(output)
-    return P, D
+    D[n:n+s], P[n:n+s,:s] = np.linalg.eig(output)
 
-@numba.njit("Tuple((f8[:, :, :], f8[:, :], i4[:]))(i4, i4[:], i4[:], f8[:], i4[:], i4[:])")
+@numba.njit("Tuple((f8[:, :], f8[:]))(i4, i4[:], i4[:], f8[:], i4[:], i4[:])")
 def _factorize_parallel(blocks, Lp, Li, Lx, Zp, Zi):
     """
     `L` is n-by-n sparse lower-triangular Cholesky factor in CSC format.
@@ -68,17 +74,17 @@ def _factorize_parallel(blocks, Lp, Li, Lx, Zp, Zi):
     The eigenvalues are stored in a matrix left-indexed by block, that is padded
     with np.nan.
     """
-    dim = Zp.size - 1
-    block_coord = np.linspace(0, dim, blocks + 1).astype(np.int32)
+    obs_dim = Zp.size - 1
+    block_coord = np.linspace(0, obs_dim, blocks + 1).astype(np.int32)
     block_size = np.diff(block_coord)
     assert np.all(block_size > 1), "Use fewer blocks"
     output_dim = np.max(block_size)
-    eigvec = np.full((blocks, output_dim, output_dim), np.nan)
-    eigval = np.full((blocks, output_dim), np.nan)
+    eigvec = np.zeros((obs_dim, output_dim))
+    eigval = np.zeros(obs_dim)
     for i in range(blocks): # TODO: parallelize with prange
-        j, m = block_coord[i], block_size[i]
-        eigvec[i, :m, :m], eigval[i, :m] = _factorize_worker(j, m, Lp, Li, Lx, Zp, Zi)
-    return eigvec, eigval, block_size
+        n, s = block_coord[i], block_size[i]
+        _factorize_worker(n, s, Lp, Li, Lx, Zp, Zi, eigvec, eigval)
+    return eigvec, eigval
 
 # --- implm
 
@@ -90,9 +96,11 @@ tau = 0.6
 y = np.ones(Zt.shape[1])
 
 # a single block (all samples)
-P, D, _ = _factorize_parallel(1, Tt.indptr, Tt.indices, Tt.data, Zt.indptr, Zt.indices)
-D = D[0]
-P = P[0]
+st = time.time()
+P, D = _factorize_parallel(1, Tt.indptr, Tt.indices, Tt.data, Zt.indptr, Zt.indices)
+en = time.time()
+print(f"Timing {en - st}")
+
 inv = P @ np.diag(1.0 / (D * tau + sigma)) @ P.T # precision matrix (not needed)
 resid = (P.T @ y) / np.sqrt(D * tau + sigma) # quadratic in loglik
 logdet = np.sum(np.log(D * tau + sigma)) # logdet in loglik

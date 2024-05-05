4f29943bd5240c56fdf5175b52429b61b1fa3022
hanbin973 <hanbin973@gmail.com>
2024-05-05 13:26:20 +0900

    sparse back/forward triangular solve for F-order array

--- tsblup/linalg.py
+++ tsblup/linalg.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numba
+from numba import prange
 
 @numba.njit("void(i4[:], i4[:], f8[:], f8[:,:])")
 def back_2dsolve_c(Lp, Li, Lx, Y):
@@ -23,6 +24,32 @@ def forward_2dsolve_c(Lp, Li, Lx, Y):
     X = Y
     for j in range(X.shape[0] - 1, -1, -1):
         for k in range(X.shape[1]):
+            for p in range(Lp[j] + 1, Lp[j + 1]):
+                X[j,k] -= Lx[p] * X[Li[p],k]
+            X[j,k] /= Lx[Lp[j]]
+
+@numba.njit("void(i4[:], i4[:], f8[:], f8[:,:])", parallel=True)
+def back_2dsolve_f(Lp, Li, Lx, Y):
+    """
+    `L` is lower-triangular Cholesky factor in CSC format: solve `L X = Y`.
+    `Y` is updated in-place.
+    """
+    X = Y
+    for k in prange(X.shape[1]):
+        for j in range(0, X.shape[0]):
+            X[j,k] /= Lx[Lp[j]]
+            for p in range(Lp[j] + 1, Lp[j + 1]):
+                X[Li[p],k] -= Lx[p] * X[j,k]
+
+@numba.njit("void(i4[:], i4[:], f8[:], f8[:,:])", parallel=True)
+def forward_2dsolve_f(Lp, Li, Lx, Y):
+    """
+    `L` is lower-triangular Cholesky factor in CSC format: solve `L' X = Y`.
+    `Y` is updated in-place.
+    """
+    X = Y
+    for k in prange(X.shape[1]):
+        for j in range(X.shape[0] - 1, -1, -1):
             for p in range(Lp[j] + 1, Lp[j + 1]):
                 X[j,k] -= Lx[p] * X[Li[p],k]
             X[j,k] /= Lx[Lp[j]]

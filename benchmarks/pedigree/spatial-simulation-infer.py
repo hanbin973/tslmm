d9e650e64c35655a04db9075f7be8e09886cd5a4
Nate Pope <nspope@utexas.edu>
2025-06-24 10:33:52 -0700

    Add dating step

--- benchmarks/pedigree/spatial-simulation-infer.py
+++ benchmarks/pedigree/spatial-simulation-infer.py
@@ -2,9 +2,12 @@ import tskit
 import tszip
 import tsinfer
 import os
+import json
 import numpy as np
 import logging
 import argparse
+
+import matplotlib.pyplot as plt
 
 
 docstring = \
@@ -24,10 +27,6 @@ if __name__ == "__main__":
         "--num-threads", type=int, default=50,
         help="Number of threads for inference",
     )
-    #parser.add_argument(
-    #    "--num-individuals", type=int, default=5000,
-    #    help="Number of individuals to subsample for inference",
-    #)
     parser.add_argument(
         "--overwrite", action="store_true",
         help="Overwrite all output",
@@ -48,14 +47,9 @@ if __name__ == "__main__":
     ts = tszip.decompress(genealogies_path)
     logging.info(f"Input tree sequence:\n{ts}")
 
-    ## subset individuals
-    #subset_path = args.inp_prefix + ".genealogies.subset.trees"
-    #subset = np.array([ind.nodes for ind in ts.individuals() if ind.time == 0])
-    #stride = subset.shape[0] // min(ts.num_samples * 2, args.num_individuals)
-    #subset = subset[::stride].flatten()
-    #ts = ts.simplify(samples=subset)
-    #logging.info(f"Subset tree sequence:\n{ts}")
-    #tszip.compress(ts, subset_path)
+    sim_mutations = json.loads(ts.provenance(-1).record)["parameters"]
+    assert sim_mutations["command"] == "sim_mutations"
+    mutation_rate = sim_mutations["rate"]
 
     # remove internal pedigree individuals, as tsinfer will choke
     sampled_individuals = \
@@ -76,3 +70,77 @@ if __name__ == "__main__":
         sample_data = tsinfer.SampleData.from_tree_sequence(ts)
         ts_inf = tsinfer.infer(sample_data, num_threads=args.num_threads)
         tszip.compress(ts_inf, inferred_path)
+    else:
+        ts_inf = tszip.load(inferred_path)
+
+    # tsdate
+    dated_path = args.inp_prefix + ".inferred.dated.trees"
+    if not os.path.exists(dated_path) or args.overwrite:
+        import tsdate
+        ts_date = tsdate.date(
+            tsdate.preprocess_ts(ts_inf),
+            mutation_rate=mutation_rate,
+            rescaling_intervals=20000,
+        )
+        tszip.compress(ts_date, dated_path)
+    else:
+        ts_date = tszip.load(dated_path)
+
+
+    # sanity checks
+    fig, axs = plt.subplots(1, 2, figsize=(8, 3), constrained_layout=True)
+
+    # plot mutation ages, frequency
+    sites_mutations = np.bincount(ts_date.mutations_site, minlength=ts_date.num_sites)
+    positions_map = {p: i for i, p in enumerate(ts_date.sites_position[sites_mutations == 1])}
+    dated_mutation_ages = np.full(len(positions_map), np.nan)
+    dated_mutation_freq = np.full(len(positions_map), -1)
+    true_mutation_ages = np.full(len(positions_map), np.nan)
+    true_mutation_freq = np.full(len(positions_map), -1)
+
+    for t in ts.trees():
+        for m in t.mutations():
+            if m.edge == tskit.NULL: continue
+            p = ts.sites_position[m.site]
+            if p in positions_map:
+                i = positions_map[p]
+                true_mutation_ages[i] = (t.time(t.parent(m.node)) + t.time(m.node)) / 2
+                true_mutation_freq[i] = t.num_samples(m.node)
+
+    for t in ts_date.trees():
+        for m in t.mutations():
+            if m.edge == tskit.NULL: continue
+            p = ts_date.sites_position[m.site]
+            if p in positions_map:
+                i = positions_map[p]
+                dated_mutation_ages[i] = (t.time(t.parent(m.node)) + t.time(m.node)) / 2
+                dated_mutation_freq[i] = t.num_samples(m.node)
+
+    xm = true_mutation_ages.mean()
+    axs[0].hexbin(true_mutation_ages, dated_mutation_ages, xscale="log", yscale="log", mincnt=1)
+    axs[0].axline((xm, xm), (xm + 1, xm + 1), color="red", linestyle="dashed")
+    axs[0].set_xlabel("true mutation age")
+    axs[0].set_ylabel("inferred mutation age")
+
+    # relatedness
+    dated_relatedness = ts_date.genetic_relatedness_vector(
+        np.eye(ts_date.num_samples)[:, [0]],
+        mode='branch',
+        centre=False,
+    )[1:]
+    true_relatedness = ts.genetic_relatedness_vector(
+        np.eye(ts.num_samples)[:, [0]],
+        mode='branch',
+        centre=False,
+    )[1:]
+    xm = true_relatedness.mean()
+    axs[1].hexbin(true_relatedness, dated_relatedness, mincnt=1)
+    axs[1].axline((xm, xm), (xm + 1, xm + 1), color="red", linestyle="dashed")
+    axs[1].set_xlabel("true branch relatedness")
+    axs[1].set_ylabel("inferred branch relatedness")
+
+    plt.savefig(f"{args.inp_prefix}.inferred.png")
+    plt.clf()
+
+
+
